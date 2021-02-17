704af834d16b15eeffc0a404599b7ff6ee65c52e
dipesh1000 <dipesh.stha70@gmail.com>
2021-02-17 14:54:04 +0545

    new change

--- resources/views/frontend/contact/index.blade.php
+++ resources/views/frontend/contact/index.blade.php
@@ -43,7 +43,7 @@
                             <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">Captcha</label>
                                 <div class="col-md-6 pull-center">
-                                    {!! app('captcha')->display() !!}
+                                    {!! app('captcha')->display() !!} 
                                     @if ($errors->has('g-recaptcha-response'))
                                         <span class="help-block">
                                             <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
